eb9d56c07b3ddf7940f1c74f553443364a754126
sepideh Borazjani <sepide.bri@gmail.com>
2022-01-25 07:20:00 +0330

    divar-array-added

---
+++ index.js
@@ -0,0 +1,179 @@
+let divarType = [
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },  
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+    {
+        image:"https://s101.divarcdn.com/static/thumbnails/1642974982/QYt3JhI6.webp",
+        name:"پرده زبرا ترکیه ای نو سالم",
+        price:"۴۵۰٬۰۰۰ تومان",
+        time:"نیم ساعت پیش",
+        place:"نعمت‌آباد",
+    },
+]
+function renderDivar() {
+    let result = ""
+    for(let i = 0; i < divarType.length; i++ ){
+        console.log(divarType[i]);
+        result += "<li><div class='divar-content'>" + "<div class='divar-name'>" + divarType[i].name + "</div>" + "<div class='divar-price'>" + divarType[i].price + "</div>" + "<div class='divar-time'>" + divarType[i].time + "</div>" + "<div class='divar-place'>" + divarType[i].place + "</div></div><img src=" + divarType[i].image  + "></li>";
+    }
+    document.querySelector("#divar ul").innerHTML= result;
+}
+renderDivar()
